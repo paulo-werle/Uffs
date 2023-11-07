a042054554829d2e016067034a656ef8c4725d96
paulowerle <pauloeduardo840@gmail.com>
2023-11-06 21:24:51 -0300

    Atualizando nomes de pastas e arquivos

--- compact-files.rb
+++ compact-files.rb
@@ -1,55 +1,67 @@
 require 'rails'
 
-class FileCompact
+class RepositoryNames
+  # Construção da classe
   def initialize(dir)
     @dir = dir
   end
 
+  # Execução da classe
   def call
-    normalize_files(@dir)
-    remove_empty_directories(@dir)
+    remove_empty(@dir)
+    normalize(@dir)
   end
 
   private
 
-  def normalize_files(dir)
+  # Remove pastas vazias
+  def remove_empty(dir)
     Dir.glob(File.join(dir, '*')).each do |path|
-      if File.file?(path)
-        rename_file(path)
-      elsif File.directory?(path)
-        rename_folder(path)  # Renomeia a pasta primária
-        normalize_files(path)
-      end
+      next unless File.directory?(path)
+
+      remove_empty(path)
+      Dir.rmdir(path) if Dir.empty?(path)
     end
   end
 
+  # Normaliza arquivos e pastas
+  def normalize(dir)
+    Dir.glob(File.join(dir, '*')).each do |path|
+      next rename_file(path) if File.file?(path)
+
+      rename_folder(path)
+      normalize(path)
+    end
+  end
+
+  # Renomeia arquivos
   def rename_file(path)
-    base_name = File.basename(path, '.*')
-    ext_name = File.extname(path)
-
-    new_name = "#{base_name.parameterize.gsub('_', '-')}#{ext_name}"
-    new_path = File.join(File.dirname(path), new_name)
-
-    File.rename(path, new_path)
+    File.rename(path, "#{File.dirname(path)}/#{file_name(path)}#{file_extension(path)}")
   end
 
+  # Renomeia pastas
   def rename_folder(path)
-    base_name = File.basename(path)
-
-    new_name = base_name.parameterize.gsub('_', '-')
-    new_path = File.join(File.dirname(path), new_name)
-
-    File.rename(path, new_path)
+    File.rename(path, "#{File.dirname(path)}/#{folder_name(path)}")
   end
 
-  def remove_empty_directories(dir)
-    Dir.glob(File.join(dir, '*')).each do |path|
-      if File.directory?(path)
-        remove_empty_directories(path)
-        Dir.rmdir(path) if Dir.empty?(path)
-      end
-    end
+  # Estrutura nome do arquivo
+  def file_name(path)
+    name = File.basename(path, '.*')
+
+    name.parameterize.gsub('_', '-')
+  end
+
+  # Estrutura nome da pasta
+  def folder_name(path)
+    name = File.basename(path)
+
+    name.parameterize.gsub('_', '-')
+  end
+
+  # Estrutura extenção do arquivo
+  def file_extension(path)
+    File.extname(path)
   end
 end
 
-FileCompact.new(Dir.pwd).call
+RepositoryNames.new(Dir.pwd).call
